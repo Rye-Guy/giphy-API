81aad6b579c9218ede850fd9759b669fa13546e1
Rye-Guy <ryanracioppo95@gmail.com>
2018-05-13 17:07:24 -0400

    some of it working

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,35 @@
 // api key k7ghyogO0TCLwIKB6SUSY71p7om684u5
 var limit = 10; 
-console.log('Hello!');
-var gifSearch = $("#textInput").val();
+var searches = ["Baseball", "Hockey", "Pinball", "Fire", "Rainbow"];
+
+
+function makeButtons(){
+    $("#buttonArea").empty();
+
+    for(var i = 0; i < searches.length; i++){
+        var btn = $("<button>");
+        btn.addClass('searchButton');
+        btn.attr('data-search', searches[i]);
+        btn.text(searches[i]);
+        $('#buttonArea').prepend(btn);
+    }
+}
+
+
+$("#createButton").on("click", function(){
+    event.preventDefault();
+
+    var newSearch = $("#textInput").val().trim();
+
+    searches.push(newSearch);
+
+    makeButtons();
+});
 
 
 $("button").on("click", function(){
     $("#gifArea").empty();
-   //gifSearch = $(this).attr("data-search");
+    gifSearch = $(this).attr("data-search");
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q="+gifSearch+"&api_key=k7ghyogO0TCLwIKB6SUSY71p7om684u5&limit="+limit;
 
@@ -15,11 +38,11 @@ $("button").on("click", function(){
         method: "GET"
     }).then(function(response){
 
-        console.log(response);
+      //  console.log(response);
 
         var result = response.data;
 
-        console.log(result);
+       // console.log(result);
 
         for(var i = 0; i < result.length; i++){
 
